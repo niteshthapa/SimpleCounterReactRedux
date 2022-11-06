a732782b8b7490a77bc23e0020e3d58c92a5a077
niteshthapa <nitesh_thapa@ymail.com>
2022-11-06 13:29:49 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Todo from './components/todo/Todo';
+function App() {
+  return (
+    <div className="container">
+      <Todo />
+    </div>
+  );
+}
+export default App;
